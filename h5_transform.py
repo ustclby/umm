4959de649e01573a5a9ffe6f50b3f67f55528d2d
Boyu Lu <bylu@uber.com>
2017-06-05 18:16:34 -0600

    change parameter and add 2fc prototxt for testing

--- h5_transform.py
+++ h5_transform.py
@@ -4,7 +4,7 @@ import numpy as np
 import h5py
 
 x = np.load('/home/bylu/resnet_map/res_mean.npy', mmap_mode='r')
-# rgb_mean = x[0, [2,1,0], :]
+rgb_mean = x[0, [2,1,0], :]
 
 
 WD = r'/home/bylu/Train_h5/'
@@ -16,10 +16,26 @@ for fn in file_names:
         data_shape = data.shape
 
         for i in range(len(data)):
-            # data[i] = (data[i] - rgb_mean) * 0.00392157
-            data[i] = data[i] * 255
+            data[i] = (data[i] - rgb_mean) #* 0.00392157
+            # data[i] = data[i] * 255
         data = data[:,[2,1,0],:]
     with h5py.File(fn, 'w') as f:
         f.create_dataset('data', data=data, compression="gzip")
         f.create_dataset('label', data=labels, compression="gzip")
 
+
+WD = r'/home/bylu/Test_h5/'
+file_names = glob.glob(os.path.join(WD, '*.h5'))
+for fn in file_names:
+    with h5py.File(fn, 'r') as h5_file:
+        data = h5_file['data'][0:]
+        labels = h5_file['label'][0:]
+        data_shape = data.shape
+
+        for i in range(len(data)):
+            data[i] = (data[i] - rgb_mean)# * 0.00392157
+            # data[i] = data[i] * 255
+        data = data[:,[2,1,0],:]
+    with h5py.File(fn, 'w') as f:
+        f.create_dataset('data', data=data, compression="gzip")
+        f.create_dataset('label', data=labels, compression="gzip")
