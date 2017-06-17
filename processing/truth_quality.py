08e029398f4a335b3d2fb6540670ca97b1f466c5
Boyu Lu <bylu@uber.com>
2017-06-17 15:57:05 -0600

    fix bug

--- processing/truth_quality.py
+++ processing/truth_quality.py
@@ -4,10 +4,8 @@ import numpy as np
 import h5py
 import caffe
 import cv2
+import random as rd
 
-# x = np.load('/home/umm/resnet_map/res_mean.npy', mmap_mode='r')
-# rgb_mean = x[0, [2,1,0], :]
-# batch_size = 16
 root = '/opt/caffe/'
 deploy = '/home/umm/resnet_map/snap_20_deploy.prototxt'
 caffe_model= '/home/umm/resnet_map/snap_iter_15000.caffemodel'
@@ -21,41 +19,32 @@ WD = r'/home/umm/Test_h5/'
 file_names = glob.glob(os.path.join(WD, '*.h5'))
 cnt = 0
 cntw = 0
+select_num = 1
+
 for fn in file_names:
     with h5py.File(fn, 'r') as h5_file:
         data = h5_file['data'][0:]
         labels = h5_file['label'][:, 1]
         labels = list(labels)
         info = h5_file['info'][0:]
-        # net.blobs['data'].reshape(len(data),3,data.shape[2],data.shape[3])
-        cnt = cnt + len(data)
-        for i in range(len(data)):
+
+        index = np.random.choice(len(data), select_num,replace=False) # choose a random subset with size select_num
+        cnt = cnt + len(index)
+        for i in index:
+            print(i)
             net.blobs['data'].data[...] = data[i]
             out = net.forward()
             predicts = out['prob'][0]
             pre = predicts.argmax()
             if abs(pre - labels[i]) > 0.01:
                 cntw = cntw + 1
-                # print(fn)
-                # print(i)
-                img = np.transpose(data[i], (1, 2, 0))
-                img_name = '/home/umm/img/' + info[i][0:-4] + '_miscl_from_' + str(int(labels[i])) + '_to_' + str(pre) + '.jpg'
-                print(img_name)
-                cv2.imwrite(img_name, img)
-print('accuracy = %5.3f',float(cnt-cntw)/float(cnt))
+
+            img = np.transpose(data[i], (1, 2, 0))
+            img_name = '/home/umm/img/' + info[i][0:-4] + '_from_' + str(int(labels[i])) + '_to_' + str(pre) + 'with_prob_' + str(predicts[pre]) +'.jpg'
+            print(img_name)
+            cv2.imwrite(img_name, img)
+
+print('accuracy = ' + str(float(cnt-cntw)/float(cnt)))
 print('end')
 
 
-    # for i in range(len(data)):
-         #   data[i] = data[i]
-
-
-
-
-
-
-
-#b = np.mean(b)
-#g = np.mean(g)
-#r = np.mean(r)
-#z = x[0,:]
